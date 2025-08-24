77918238bcead37674b4e1b389ca428ee49ab0b7
rafaelwebber <rafael.webber09@gmail.com>
2025-08-24 15:56:15 -0300

    add o gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
 import mysql.connector
 import bcrypt
+from dotenv import load_dotenv
 
+load_dotenv()
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
     password="123456",
     database="catalago"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
 
 cursor = conn.cursor()
