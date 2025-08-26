187949f04f45df3cc1e7461660bb54a674b68097
rafaelwebber <rafael.webber09@gmail.com>
2025-08-26 14:27:14 -0300

    alguma coisa

--- backend/controllers/usuario_controller.py
+++ backend/controllers/usuario_controller.py
@@ -1,6 +1,5 @@
 from models.database import conn, cursor
 from models.resposta_padrao import RespostaPadrao
-<<<<<<< HEAD
 import bcrypt
 
 def add_usuario(nome, email, data_nascimento, senha):
@@ -13,20 +12,6 @@ def add_usuario(nome, email, data_nascimento, senha):
 
     senha_hash = bcrypt.hashpw(senha.encode(), bcrypt.gensalt())
     cursor.execute("INSERT INTO usuario (nome, email, data_nascimento, senha) VALUES (%s, %s, %s, %s)", (nome, email, data_nascimento, senha_hash))
-=======
-import hashlib
-
-def add_usuario(nome, email, senha):
-    if not nome or not email or not senha:
-        return RespostaPadrao(400, "Todos os campos são obrigatórios.").to_dict()
-
-    cursor.execute("SELECT id FROM usuarios WHERE email = %s", (email,))
-    if cursor.fetchone():
-        return RespostaPadrao(409, "Email já cadastrado.").to_dict()
-
-    senha_hash = hashlib.sha256(senha.encode()).hexdigest()
-    cursor.execute("INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)", (nome, email, senha_hash))
->>>>>>> 208880cba3f5f2081aecb6b34a15a4f31fe95439
     conn.commit()
 
     return RespostaPadrao(201, "Usuário cadastrado com sucesso.").to_dict()
@@ -36,18 +21,10 @@ def fazer_login(email, senha):
     if not email or not senha:
         return RespostaPadrao(400, "Email e senha são obrigatórios.").to_dict()
 
-<<<<<<< HEAD
     cursor.execute("SELECT id, nome, senha FROM usuario WHERE email = %s" , (email,))
     usuario = cursor.fetchone()
 
     if not usuario or not bcrypt.checkpw(senha.encode(), usuario[2].encode()):
-=======
-    senha_hash = hashlib.sha256(senha.encode()).hexdigest()
-    cursor.execute("SELECT id, nome FROM usuarios WHERE email = %s AND senha = %s", (email, senha_hash))
-    usuario = cursor.fetchone()
-
-    if not usuario:
->>>>>>> 208880cba3f5f2081aecb6b34a15a4f31fe95439
         return RespostaPadrao(401, "Credenciais inválidas.").to_dict()
 
     return RespostaPadrao(200, "Login realizado com sucesso.", dados={"id": usuario[0], "nome": usuario[1]}).to_dict()
--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,17 +1,11 @@
 from flask import Flask
 from routes.filmes_routes import filmes_bp
 from routes.usuario_routes import usuario_bp
-<<<<<<< HEAD
 from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     CORS(app) 
-=======
-
-def create_app():
-    app = Flask(__name__)
->>>>>>> 208880cba3f5f2081aecb6b34a15a4f31fe95439
     app.register_blueprint(filmes_bp)
     app.register_blueprint(usuario_bp)
     return app
